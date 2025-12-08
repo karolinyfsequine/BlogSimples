c36c73cedd823db8421c7cf2561f08c4dd35e012
Karoliny Fernandes Sequine <artesgirlsbr@gmail.com>
2025-12-08 20:05:22 -0300

    registro do CRUD de Categorias no main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from util.exception_handlers import (
 from util.exceptions import ErroValidacaoFormulario
 
 # Repositórios
-from repo import (
+from repo import (chat_sala_repo, chat_participante_repo, chat_mensagem_repo, categoria_repo,
     usuario_repo,
     configuracao_repo,
     chamado_repo,
@@ -43,6 +43,7 @@ from routes.usuario_routes import router as usuario_router
 from routes.chat_routes import router as chat_router
 from routes.public_routes import router as public_router
 from routes.examples_routes import router as examples_router
+from routes.admin_categorias_routes import router as admin_categorias_router
 
 # Seeds
 from util.seed_data import inicializar_dados
@@ -82,6 +83,7 @@ TABELAS = [
     (chat_sala_repo, "chat_sala"),
     (chat_participante_repo, "chat_participante"),
     (chat_mensagem_repo, "chat_mensagem"),
+    (categoria_repo, "categoria")
 ]
 
 # Criar tabelas do banco de dados
@@ -121,10 +123,12 @@ ROUTERS = [
     (admin_config_router, ["Admin - Configurações"], "admin de configurações"),
     (admin_backups_router, ["Admin - Backups"], "admin de backups"),
     (admin_chamados_router, ["Admin - Chamados"], "admin de chamados"),
+    (admin_categorias_router, ["Admin - Categorias"], "admin de categorias"),
     (usuario_router, ["Usuário"], "usuário"),
     (chat_router, ["Chat"], "chat"),
     (public_router, ["Público"], "público"),
     (examples_router, ["Exemplos"], "exemplos"),
+    
 ]
 
 # Incluir routers
